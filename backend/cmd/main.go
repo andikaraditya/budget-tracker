bfafb711bb4aff5d059fb54962242c491d7dee60
andikaraditya <andikaraditya1@gmail.com>
2024-07-12 20:16:24 +0700

    add logger

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,10 +5,13 @@ import (
 	"github.com/andikaraditya/budget-tracker/backend/internal/user"
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
