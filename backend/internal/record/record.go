c86c9ab99e2f3f6edc5c5db1f6c8d791fe817804
andikaraditya <andikaraditya1@gmail.com>
2024-07-13 19:19:22 +0700

    add record service

---
+++ backend/internal/record/record.go
@@ -0,0 +1,24 @@
+package record
+
+import (
+	"github.com/andikaraditya/budget-tracker/backend/internal/category"
+	"github.com/andikaraditya/budget-tracker/backend/internal/source"
+	"github.com/andikaraditya/budget-tracker/backend/internal/user"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type Record struct {
+	ID          string             `json:"id"`
+	Amount      float64            `json:"amount" validate:"required"`
+	Description string             `json:"description"`
+	Type        string             `json:"type" validate:"required,oneof=income expense"`
+	CategoryId  string             `json:"category_id,omitempty" validate:"required"`
+	Category    category.Category  `json:"category,omitempty" validate:"-"`
+	SourceId    string             `json:"source_id,omitempty" validate:"required"`
+	Source      source.Source      `json:"source" validate:"-"`
+	UserId      string             `json:"-"`
+	User        user.UserSimple    `json:"user"`
+	Date        pgtype.Timestamptz `json:"date" validate:"required"`
+	CreatedAt   pgtype.Timestamptz `json:"created_at"`
+	UpdatedAt   pgtype.Timestamptz `json:"updated_at"`
+}
---
+++ backend/internal/record/service.go
@@ -0,0 +1,244 @@
+package record
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/andikaraditya/budget-tracker/backend/internal/api"
+	"github.com/andikaraditya/budget-tracker/backend/internal/db"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type RecordService interface {
+	createRecord(req *Record) error
+	getRecords(userId string) ([]Record, error)
+	getRecord(req *Record) error
+	updateRecord(req *Record, updatedFields []string) error
+}
+
+type srv struct {
+	db db.DBService
+}
+
+var (
+	Service RecordService
+)
+
+func init() {
+	Service = New(db.Service)
+}
+
+func New(db db.DBService) RecordService {
+	return &srv{db}
+}
+
+func (s *srv) createRecord(req *Record) error {
+	if err := s.db.Commit(nil, func(tx pgx.Tx) error {
+		req.ID = uuid.NewString()
+		_, err := tx.Exec(
+			context.Background(),
+			`INSERT INTO "record" (
+				id,
+				amount,
+				description,
+				type,
+				category_id,
+				source_id,
+				user_id,
+				date
+			) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+			req.ID,
+			req.Amount,
+			req.Description,
+			req.Type,
+			req.CategoryId,
+			req.SourceId,
+			req.UserId,
+			req.Date,
+		)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return s.getRecord(req)
+}
+
+func (s *srv) getRecords(userId string) ([]Record, error) {
+	var r []Record
+	rows, err := s.db.Query(
+		`SELECT 
+			id,
+			amount,
+			description,
+			"type" ,
+			(SELECT json_build_object(
+					'id', id,
+					'name', name,
+					'type', type,
+					'description', description,
+					'created_at', created_at,
+					'updated_at', updated_at
+					) 
+					FROM "category" a
+					WHERE a.id = r.category_id
+			) AS "category",
+			(SELECT json_build_object(
+					'id', id,
+					'name', name,
+					'description', description,
+					'created_at', created_at,
+					'updated_at', updated_at
+					) 
+					FROM "source" b
+					WHERE b.id = r.source_id
+			) AS "source",
+			(SELECT json_build_object(
+					'id', id,
+					'name', name
+					) 
+					FROM "user" u
+					WHERE u.id = r.user_id
+			) AS "user",
+			"date",
+			created_at,
+			updated_at
+		FROM record r
+		WHERE r.user_id = $1`,
+		userId,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var o Record
+		if err := rows.Scan(
+			&o.ID,
+			&o.Amount,
+			&o.Description,
+			&o.Type,
+			&o.Category,
+			&o.Source,
+			&o.User,
+			&o.Date,
+			&o.CreatedAt,
+			&o.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		r = append(r, o)
+	}
+	return r, nil
+}
+func (s *srv) getRecord(req *Record) error {
+	if err := s.db.QueryRow(
+		`SELECT 
+			id,
+			amount,
+			description,
+			"type" ,
+			(SELECT json_build_object(
+					'id', id,
+					'name', name,
+					'type', type,
+					'description', description,
+					'created_at', created_at,
+					'updated_at', updated_at
+					) 
+					FROM "category" a
+					WHERE a.id = r.category_id
+			) AS "category",
+			(SELECT json_build_object(
+					'id', id,
+					'name', name,
+					'description', description,
+					'created_at', created_at,
+					'updated_at', updated_at
+					) 
+					FROM "source" b
+					WHERE b.id = r.source_id
+			) AS "source",
+			(SELECT json_build_object(
+					'id', id,
+					'name', name
+					) 
+					FROM "user" u
+					WHERE u.id = r.user_id
+			) AS "user",
+			"date",
+			created_at,
+			updated_at
+		FROM record r
+		WHERE r.id = $1 AND r.user_id = $2`,
+		req.ID,
+		req.UserId,
+	).Scan(
+		&req.ID,
+		&req.Amount,
+		&req.Description,
+		&req.Type,
+		&req.Category,
+		&req.Source,
+		&req.User,
+		&req.Date,
+		&req.CreatedAt,
+		&req.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return api.ErrNotFound
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (s *srv) updateRecord(req *Record, updatedFields []string) error {
+	if err := s.db.Commit(nil, func(tx pgx.Tx) error {
+		req.UpdatedAt = pgtype.Timestamptz{Time: time.Now(), Valid: true}
+		args := []any{req.ID, req.UpdatedAt}
+		var sb strings.Builder
+
+		for _, field := range updatedFields {
+			switch field {
+			case "amount":
+				args = append(args, req.Amount)
+				sb.WriteString(fmt.Sprintf("amount = $%d,", len(args)))
+			case "description":
+				args = append(args, req.Description)
+				sb.WriteString(fmt.Sprintf("description = $%d,", len(args)))
+			case "date":
+				args = append(args, req.Date)
+				sb.WriteString(fmt.Sprintf("date = $%d,", len(args)))
+			}
+		}
+
+		if len(args) > 2 {
+			if _, err := tx.Exec(
+				context.Background(),
+				fmt.Sprintf(
+					`UPDATE record
+					SET %s
+						updated_at = $2
+					WHERE id = $1`,
+					sb.String(),
+				),
+				args...,
+			); err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return s.getRecord(req)
+}
